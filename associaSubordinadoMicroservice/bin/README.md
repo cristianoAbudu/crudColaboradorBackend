072bf504c4b9aade4759f14395dd86a1abe1a1a3
jovemTranquilao <jovem@hocem.com>
2023-08-07 18:57:09 -0300

    separando em microservicos

---
+++ associaSubordinadoMicroservice/bin/README.md
@@ -0,0 +1,6 @@
+Backend spring boot
+
+Requer apenas um MySql rodando com usuário root e senha 1234567
+
+conforme especificado no arquivo application.properties
+
---
+++ findAllMicroservice/src/main/java/com/backend/BackendApplication.java
@@ -0,0 +1,57 @@
+package com.backend;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*");
+			}
+		};
+	}
+
+	@Bean
+	Queue queue() {
+		return new Queue("spring-boot", false);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange("spring-boot-exchange");
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+	}
+
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames("spring-boot");
+		return container;
+	}
+
+
+}
