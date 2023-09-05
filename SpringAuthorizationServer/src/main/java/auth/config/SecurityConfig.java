2739785af252963566a3058af233703ad5a96083
lucas.lima <ldslima@rd.com.br>
2023-09-05 20:53:41 -0300

    Uso adequado do OAuth2

---
+++ SpringAuthorizationServer/src/main/java/auth/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+		var user1 = User.withUsername("user").password("{noop}password").roles("USER").build();
+		var user2 = User.withUsername("admin").password("{noop}password").roles("USER", "ADMIN").build();
+		return new InMemoryUserDetailsManager(user1, user2);
+	}
+
+	@Bean
+	OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
+		return context -> {
+			Authentication principal = context.getPrincipal();
+			if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
+				Set<String> authorities = principal.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+						.collect(Collectors.toSet());
+				context.getClaims().claim("authorities", authorities);
+			}
+		};
+	}
+
+}
