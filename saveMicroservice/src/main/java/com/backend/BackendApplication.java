fc6c22eab791f6480957e6cda4efbcda62bd8048
jovemTranquilao <jovem@hocem.com>
2023-09-08 19:57:16 -0300

    Cors liberado falta passsar o token

--- saveMicroservice/src/main/java/com/backend/BackendApplication.java
+++ saveMicroservice/src/main/java/com/backend/BackendApplication.java
@@ -1,8 +1,13 @@
 package com.backend;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -38,5 +43,15 @@ public class BackendApplication {
 	 * container.setConnectionFactory(connectionFactory);
 	 * container.setQueueNames("spring-boot"); return container; }
 	 */
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 
 }
--- saveMicroservice/src/main/java/com/backend/controller/ColaboradorController.java
+++ saveMicroservice/src/main/java/com/backend/controller/ColaboradorController.java
@@ -1,20 +1,32 @@
 package com.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.business.ColaboradorBusiness;
 import com.backend.entity.ColaboradorEntity;
 
-@RestController(value = "/colaborador")
+@CrossOrigin(origins = "*")
+@RestController
 public class ColaboradorController {
+	
+	/*
+	 * 
+	 * 
+7:37 PM
+Lucas Alves
+​jovem, uma vez eu fiz colocando uma config no application.yml -> "cors: originPatterns:" e ai coloquei as urls e depois chamei o decorator em cada metodo igual ce fez no controller
+	 */
 	
 	@Autowired
 	ColaboradorBusiness colaboradorBusiness;
 	
-	@PostMapping
+	//@CrossOrigin
+    @RequestMapping(method = RequestMethod.POST, path = "/{id}")
 	public ColaboradorEntity post(@RequestBody ColaboradorEntity colaboradorEntity) throws Exception {
 		return  colaboradorBusiness.save(colaboradorEntity);
 	}
